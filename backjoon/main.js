0392412f89c0c71a80a33ac2925849126a4a4676
sieun <sieun0115@ajou.ac.kr>
2021-09-30 15:10:06 +0900

    오타 수정

--- backjoon/main.js
+++
@@ -1,4 +0,0 @@
-// const input =  require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const input =``.split('\n').map(x => Number(x));
-
---
+++ baekjoon/main.js
@@ -0,0 +1,13 @@
+// const input =  require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const input =`(()[[]])([])`;
+
+let cnt1=0;
+let cnt2=0;
+let before;
+for(let i=0; i<input.length;i++){
+    
+}
+
+
+
