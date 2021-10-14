2c71e25a8198f6bca7e4e2c70d0f4b895571fcbd
sieun <sieun0115@ajou.ac.kr>
2021-10-14 18:19:36 +0900

    feat: priority queue complete

--- 자료구조+알고리즘/priority_queue.js
+++ 자료구조+알고리즘/priority_queue.js
@@ -1,24 +1,44 @@
+class QueueElement {
+    constructor(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
 class Queue {
     constructor() {
         this.items = [];
         this.count = 0;
     }
-    enqueue(element) {
-        this.items[this.count] = element;
-        console.log(`queue[${this.count}]: ${element} enqueued`);
+    enqueue(element, priority) {
+        let queueEl = new QueueElement(element, priority);
+        let isEuqueued = false;
+        for (let i = 0; i < this.count; i++) {
+            if (queueEl.priority < this.items[i].priority) {
+                this.items.splice(i, 0, queueEl);
+                isEuqueued = true;
+                console.log(`priority queue[${this.count}]: ${element} enqueued`);
+                break;
+            }
+        }
+        if(!isEuqueued){
+            this.items[this.count] = queueEl;
+            console.log(`queue[${this.count}]: ${element} enqueued`);
+        }
+        
         this.count++;
         return this.count-1;
     }
     dequeue(){
         if(this.count===0) return undefined;
-        let toDequeue = this.items[0];
+        let toDequeue = this.items[0].element;
         this.count--;
         this.items.shift();
         console.log(`queue[0]: ${toDequeue} has dequeued`);
         return toDequeue;
     }
     front() {
-        console.log(`queue[front]: ${this.items[0]}`);
+        console.log(`queue[front]: ${this.items[0].element}`);
         return this.items[0];
     }   
     isEmpty() {
@@ -30,7 +50,7 @@ class Queue {
         return this.count;
     }
     print(){
-        let toPrint = this.items.toString();
+        let toPrint = [...this.items];
         console.log(toPrint);
         return toPrint;
     }
@@ -41,3 +61,16 @@ class Queue {
         return this.items;
     }
 }
+
+const queue = new Queue;
+queue.enqueue('가', 4);
+queue.enqueue('나', 0);
+queue.enqueue('다',2);
+queue.size();
+queue.enqueue('라', 1);
+queue.enqueue('마',10);
+queue.size();
+queue.dequeue();
+queue.size();
+queue.print();
+
