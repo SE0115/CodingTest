6efdf03ea81ff760673ece962e7a2f5fbe549352
sieun <sieun0115@ajou.ac.kr>
2021-10-14 16:53:48 +0900

    queue

---
+++ 자료구조+알고리즘/queue.js
@@ -0,0 +1,55 @@
+class Queue {
+    constructor() {
+        this.items = [];
+        this.count = 0;
+    }
+    enqueue(element) {
+        this.items[this.count] = element;
+        console.log(`queue[${this.count}]: ${element} enqueued`);
+        this.count++;
+        return this.count-1;
+    }
+    dequeue(){
+        if(this.count===0) return undefined;
+        let toDequeue = this.items[0];
+        this.count--;
+        this.items.shift();
+        console.log(`queue[0]: ${toDequeue} has dequeued`);
+        return toDequeue;
+    }
+    front() {
+        console.log(`queue[front]: ${this.items[0]}`);
+        return this.items[0];
+    }   
+    isEmpty() {
+        console.log(this.count===0);
+        return this.count===0
+    }
+    size(){
+        console.log(`queue size: ${this.count}`);
+        return this.count;
+    }
+    print(){
+        let toPrint = this.items.toString();
+        console.log(toPrint);
+        return toPrint;
+    }
+    clear(){
+        this.items.splice(0,this.items.length);
+        this.count = 0;
+        console.log('Reset queue complete');
+        return this.items;
+    }
+}
+
+const queue = new Queue();
+queue.isEmpty();
+queue.enqueue('java');
+queue.enqueue('Script');
+queue.dequeue();
+queue.enqueue('C');
+queue.enqueue('python');
+queue.print();
+queue.size();
+queue.clear();
+queue.isEmpty();
