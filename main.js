2b3427462b1d9b1de4af1c118ac07099a10d2c18
sieun <sieun0115@ajou.ac.kr>
2021-10-23 22:34:42 +0900

    21.10.23

--- main.js
+++ main.js
@@ -17,47 +17,32 @@ push 3
 empty
 front`.split('\n');
 
-class Queue {
-  constructor() {
-    this.queue=[];
-    this.size=0;
-  }
-  push(data) {
-    this.queue.push(data);
-    this.size++;
-  }
-  pop() {
-    if(!this.size) {
-      return -1;
-    }
-    const popped = this.queue.shift();
-    this.size--;
-    return popped;
-  }
-  isEmpty() {
-    return (!this.size ? 1 : 0);
-  }
-  front() {
-    return (!this.size ? -1 : this.queue[0]);
-  }
-  back() {
-    return (!this.size ? -1 : this.queue[this.size-1]);
-  }
-}
 
 const len = input.shift();
-const queue = new Queue();
+let size = 0;
+let queue = [];
 let result = [];
 
 for(let i=0;i<len;i++) {
   let command = input[i].split(' ');
   switch(command[0]) {
-    case "push" : queue.push(command[1]); break;
-    case "pop" : result.push(queue.pop()); break;
-    case "size" : result.push(queue.size); break;
-    case "empty" : result.push(queue.isEmpty()); break;
-    case "front" : result.push(queue.front()); break;
-    case "back" : result.push(queue.back()); break;
+    case "push" : 
+      queue.push(command[1]);
+      size++;
+      break;
+    case "pop" : 
+      if(size>0) {
+        result.push(queue.shift());
+        size--;
+      }
+      else {
+        result.push(-1);
+      }
+      break;
+    case "size" : result.push(size); break;
+    case "empty" : result.push(size ? 0 : 1); break;
+    case "front" : result.push(size ? queue[0] : -1); break;
+    case "back" : result.push(size ? queue[size-1] : -1); break;
   }
 }
 
