1d9e209c16ea337737ef1924dbb9f2c647246d74
sieun <sieun0115@ajou.ac.kr>
2021-10-24 22:47:18 +0900

    21.10.24

--- main.js
+++ main.js
@@ -17,6 +17,49 @@ push 3
 empty
 front`.split('\n');
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.size = 0;
+  }
+  size() {
+    return this.size;
+  }
+  push(data) {
+    const newNode = new Node(data);
+    if(this.size===0) {
+      this.front = newNode;
+      this.back = newNode;
+    }
+    else{
+      let temp = this.back;
+      console.log(temp);
+      this.back = newNode;
+      temp.next = newNode;
+    }
+    this.size++;
+  }
+  pop() {
+    
+  }
+  print() {
+    let head = this.front;
+    let list = [];
+    while(head) {
+      list.push(head.data);
+      head = head.next;
+    }
+    console.log(list.join('-> '));
+  }
+}
 
 const len = input.shift();
 let size = 0;
@@ -46,6 +89,11 @@ for(let i=0;i<len;i++) {
   }
 }
 
-console.log(result.join('\n'));
-
-// 시간초과
+let queue_data = new Queue;
+queue_data.push(1);
+queue_data.push(2);
+queue_data.push(3);
+queue_data.push(4);
+queue_data.push(5);
+queue_data.push(6);
+queue_data.print(7);
