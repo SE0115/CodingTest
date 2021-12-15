28d23f16f953eb3ad89618a4e33c461fd655fd75
sieun <sieun0115@ajou.ac.kr>
2021-12-15 21:41:38 +0900

    Refactor: queue

--- 자료구조+알고리즘/queue.js
+++ 자료구조+알고리즘/queue.js
@@ -1,55 +1,41 @@
 class Queue {
-    constructor() {
-        this.items = [];
-        this.count = 0;
-    }
-    enqueue(element) {
-        this.items[this.count] = element;
-        console.log(`queue[${this.count}]: ${element} enqueued`);
-        this.count++;
-        return this.count-1;
-    }
-    dequeue(){
-        if(this.count===0) return undefined;
-        let toDequeue = this.items[0];
-        this.count--;
-        this.items.shift();
-        console.log(`queue[0]: ${toDequeue} has dequeued`);
-        return toDequeue;
-    }
-    front() {
-        console.log(`queue[front]: ${this.items[0]}`);
-        return this.items[0];
-    }   
-    isEmpty() {
-        console.log(this.count===0);
-        return this.count===0
-    }
-    size(){
-        console.log(`queue size: ${this.count}`);
-        return this.count;
-    }
-    print(){
-        let toPrint = this.items.toString();
-        console.log(toPrint);
-        return toPrint;
-    }
-    clear(){
-        this.items.splice(0,this.items.length);
-        this.count = 0;
-        console.log('Reset queue complete');
-        return this.items;
-    }
+  constructor() {
+    this.items = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+  enqueue(item) {
+    this.items[this.tail] = item;
+    console.log(`queue[${this.tail}]: ${item} enqueued`);
+    this.tail++;
+  }
+  dequeue() {
+    if (this.tail === 0) return undefined;
+    const item = this.items[this.head];
+    delete this.items[this.head];
+    console.log(`queue[0]: ${item} has dequeued`);
+    this.head++;
+    return item;
+  }
+  front() {
+    console.log(`queue[front]: ${this.items[this.head]}`);
+    return this.items[this.head];
+  }
+  isEmpty() {
+    console.log(this.tail - this.head === 0);
+    return this.tail - this.head === 0;
+  }
+  size() {
+    console.log(`queue size: ${this.tail - this.head}`);
+    return this.tail - this.head;
+  }
+  print() {
+    console.log(Object.entries(this.items));
+  }
+  clear() {
+    Object.keys(this.items).map((item) => delete this.items[item]);
+    this.head = 0;
+    this.tail = 0;
+    console.log("Reset queue complete");
+  }
 }
-
-const queue = new Queue();
-queue.isEmpty();
-queue.enqueue('java');
-queue.enqueue('Script');
-queue.dequeue();
-queue.enqueue('C');
-queue.enqueue('python');
-queue.print();
-queue.size();
-queue.clear();
-queue.isEmpty();
